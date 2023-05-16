61fc3d3aa8227317c67bf9ccebb3b1393221aebb
Saniatul Haque Taposh <taposhmd2@gmail.com>
2023-05-16 20:00:52 +0600

    up

--- resources/views/admin/blog/add-blog.blade.php
+++ resources/views/admin/blog/add-blog.blade.php
@@ -4,7 +4,7 @@
         <div class="row justify-content-center">
             <div class="col-lg-7">
                 <div class="card shadow-lg border-0 rounded-lg mt-5">
-                    <div class="card-header"><h3 class="text-center font-weight-light my-4">New Blog</h3></div>
+                    <div class="card-header"><h3 class="text-center font-weight-light my-4">create Blog</h3></div>
                     <div class="card-body">
                         <form action="{{ route('new-blog') }}" method="post" enctype="multipart/form-data">
                             @csrf
@@ -36,7 +36,7 @@
                             </div>
                             <div class="form-floating mb-3">
                                 <input class="form-control" id="inputEmail" type="text" name="title" placeholder="Title" />
-                                <label for="inputEmail">Total Title</label>
+                                <label for="inputEmail">Blogs Title</label>
                             </div>
                             <div class="form-floating mb-3">
                                 <input class="form-control" id="inputEmail" type="text" name="slug" placeholder="slug" />
