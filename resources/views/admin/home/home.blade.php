708e3afdf1891ff453fea8515058a3646ce4333a
Saniatul Haque Taposh <taposhmd2@gmail.com>
2023-07-21 20:30:32 +0600

    dashboard update

--- resources/views/admin/home/home.blade.php
+++ resources/views/admin/home/home.blade.php
@@ -107,43 +107,20 @@
                         <td>2011/07/25</td>
                         <td>$170,750</td>
                     </tr>
+
                     <tr>
-                        <td>Ashton Cox</td>
-                        <td>Junior Technical Author</td>
-                        <td>San Francisco</td>
-                        <td>66</td>
-                        <td>2009/01/12</td>
-                        <td>$86,000</td>
-                    </tr>
-                    <tr>
-                        <td>Cedric Kelly</td>
-                        <td>Senior Javascript Developer</td>
-                        <td>Edinburgh</td>
-                        <td>22</td>
-                        <td>2012/03/29</td>
-                        <td>$433,060</td>
-                    </tr>
-                    <tr>
-                        <td>Airi Satou</td>
-                        <td>Accountant</td>
-                        <td>Tokyo</td>
-                        <td>33</td>
-                        <td>2008/11/28</td>
-                        <td>$162,700</td>
-                    </tr>
-                    <tr>
-                        <td>Brielle Williamson</td>
+                        <td>willims vai</td>
                         <td>Integration Specialist</td>
-                        <td>New York</td>
+                        <td>New York City</td>
                         <td>61</td>
                         <td>2012/12/02</td>
                         <td>$372,000</td>
                     </tr>
                     <tr>
-                        <td>Herrod Chandler</td>
+                        <td>Herrod Chaldal</td>
                         <td>Sales Assistant</td>
                         <td>San Francisco</td>
-                        <td>59</td>
+                        <td>598</td>
                         <td>2012/08/06</td>
                         <td>$137,500</td>
                     </tr>
@@ -172,7 +149,7 @@
                         <td>$103,600</td>
                     </tr>
                     <tr>
-                        <td>sani Gaines</td>
+                        <td>sani Vai</td>
                         <td>Office Manager</td>
                         <td>London</td>
                         <td>30</td>
